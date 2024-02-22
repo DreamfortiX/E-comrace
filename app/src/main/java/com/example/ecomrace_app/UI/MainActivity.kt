92ced06037579ff8e9bd8f53776072da5f8bd419
hp <haseebjutt3557@gmail.com>
2024-02-22 09:25:18 +0500

    Made a e comrace app

---
+++ app/src/main/java/com/example/ecomrace_app/UI/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecomrace_app.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecomrace_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
